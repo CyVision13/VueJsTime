f576f2badd5724f3ba06ad2bc4433d4cc8eb95eb
Cyvision <cyvision13@outlook.com>
2020-12-14 18:29:23 +0330

    008-CustomEvents

---
+++ components/EventsChild.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+      
+      <p>event child</p>
+      <button @click="submit">Submit</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'EventsChild',
+    methods:{
+        submit(){
+            setTimeout( () => {
+                this.$emit('onSuccess','my custom param')
+            }, 1000);
+        
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/events.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+       <events-child @onSuccess="onSuccess"></events-child>
+  </div>
+</template>
+
+<script>
+export default {
+name: 'Events',
+    methods:{
+        onSuccess(param){
+            console.log('success',param);
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
